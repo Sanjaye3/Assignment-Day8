02d2524f673dcd68fff1a46a87a37f47880ce833
Sanjaye3 <92200293+Sanjaye3@users.noreply.github.com>
2022-01-21 12:04:45 +0530

    Add files via upload

---
+++ uberprice.js
@@ -0,0 +1,13 @@
+class uberfare{
+    constructor(basefare,costperminute,timeinride,costperkm,ridedistance,bookingfee){
+        this.basefare=basefare;
+        this.costperminute=costperminute;
+        this.timeinride=timeinride;
+        this.costperkm=costperkm;
+        this.ridedistance=ridedistance;
+        this.bookingfee=bookingfee;
+        let value=basefare+(costperminute*timeinride)+(costperkm*ridedistance)+bookingfee;
+        console.log("the uber fee is" + value);
+    }
+}
+let obj=new uberfare(100,10,10,10,10,25);
